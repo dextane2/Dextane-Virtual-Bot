e5b050d105b282e4e298766ff8cefc952c8ed295
dextane2 <captinosalvini@gmail.com>
2025-10-22 11:52:30 -0400

    Delete install_playwright.py

--- install_playwright.py
+++
@@ -1,9 +0,0 @@
-from playwright.sync_api import sync_playwright
-
-print("Installing Chromium for Playwright...")
-
-with sync_playwright() as p:
-    for browser_type in [p.chromium]:
-        browser_type.install()
-
-print("✅ Chromium installation complete.")
